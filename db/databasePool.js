253a8a4e077d8bb0309740c214ab785761778036
normanjayasubrata <norman.jayasubrata@gmail.com>
2020-05-08 22:41:25 +0700

    db config update

--- db/databasePool.js
+++ db/databasePool.js
@@ -1,6 +1,9 @@
 const {Pool} = require("pg");
 const config = process.env.DATABASE_URL || require("../secrets/config");
 
-const pool = new Pool(config)
+const pool = new Pool({
+   ...config,
+    password: ""
+})
 
 module.exports = pool;
