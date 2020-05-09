4388c83987fbf7adca2d3c8ca498e5e09d7cb2c9
normanjayasubrata <norman.jayasubrata@gmail.com>
2020-05-09 14:12:16 +0700

    env config setting

--- db/knex.js
+++ db/knex.js
@@ -1,5 +1,6 @@
 const environtment = process.env.NODE_ENV || "development";
 const config = require("../knexfile");
+const environtmentConfig = config[environtmentConfig]
 
-module.exports = require("knex")(config[environtment]);
+module.exports = require("knex")(environtmentConfig);
 
