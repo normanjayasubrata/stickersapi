28032ea6e4aac29ff008a2b2c68790b303bfc7c3
normanjayasubrata <norman.jayasubrata@gmail.com>
2020-05-09 13:48:02 +0700

    knex modif

--- db/knex.js
+++ db/knex.js
@@ -1,12 +1,8 @@
-console.log("norman", process.env.NODE_ENV)
 const environtment = process.env.NODE_ENV || "development";
-console.log("environtment", environtment)
-
 const config = require("../knexfile");
-console.log("config", config)
+const environtmentConfig = config[environtment];
+const knex = require("knex");
 
-const knex = require("knex")(config[environtment]);
-console.log("knex", knex)
 
-module.exports = knex
+module.exports = knex(environtmentConfig);
 
