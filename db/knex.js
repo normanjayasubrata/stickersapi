c94543a7cbb972bcf3cf519afacdca756bdcca29
normanjayasubrata <norman.jayasubrata@gmail.com>
2020-05-09 14:17:26 +0700

    fix env

--- db/knex.js
+++ db/knex.js
@@ -1,6 +1,5 @@
 const environtment = process.env.NODE_ENV || "development";
 const config = require("../knexfile");
-const environtmentConfig = config[environtment]
 
-module.exports = require("knex")(environtmentConfig);
+module.exports = require("knex")(config[environtment]);
 
